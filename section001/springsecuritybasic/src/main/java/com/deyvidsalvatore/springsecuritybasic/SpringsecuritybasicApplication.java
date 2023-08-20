bfa30451c3788556ed1b6712381e53e8490e0ba7
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-08-20 13:03:20 -0300

    backup

---
+++ section001/springsecuritybasic/src/main/java/com/deyvidsalvatore/springsecuritybasic/SpringsecuritybasicApplication.java
@@ -0,0 +1,13 @@
+package com.deyvidsalvatore.springsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybasicApplication.class, args);
+	}
+
+}
---
+++ section001/springsecuritybasic/src/main/java/com/deyvidsalvatore/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.deyvidsalvatore.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Spring Boot World";
+	}
+
+}
