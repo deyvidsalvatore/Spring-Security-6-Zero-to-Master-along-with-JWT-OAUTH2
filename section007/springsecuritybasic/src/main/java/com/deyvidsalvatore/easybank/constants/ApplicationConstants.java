a2501fbc92227d7b68b3342fa422c58051079837
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-08-23 14:48:24 -0300

    section 7 code

---
+++ section007/springsecuritybasic/src/main/java/com/deyvidsalvatore/easybank/constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.deyvidsalvatore.easybank.constants;
+
+public class ApplicationConstants {
+
+    public static final String ERROR_MSG = "An unknown error occured";
+    public static final String SUCCESS_MSG = "Request Successfully processed";
+
+}
---
+++ section007/springsecuritybasic/src/main/java/com/deyvidsalvatore/easybank/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.deyvidsalvatore.easybank.repository;
+
+import com.deyvidsalvatore.easybank.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByEmail(String email);
+    
+}
