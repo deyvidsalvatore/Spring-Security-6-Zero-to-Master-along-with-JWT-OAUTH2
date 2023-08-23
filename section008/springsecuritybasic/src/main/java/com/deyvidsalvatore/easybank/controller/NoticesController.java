6436cc83a59e748ec9007ddd6bbbc17693a16b5d
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-08-23 14:52:24 -0300

    section 8 code

---
+++ section008/springsecuritybasic/src/main/java/com/deyvidsalvatore/easybank/controller/NoticesController.java
@@ -0,0 +1,32 @@
+package com.deyvidsalvatore.easybank.controller;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.deyvidsalvatore.easybank.model.Notice;
+import com.deyvidsalvatore.easybank.repository.NoticeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+
+    @Autowired
+    private NoticeRepository noticeRepository;
+
+    @GetMapping("/notices")
+    public ResponseEntity<List<Notice>> getNotices() {
+        List<Notice> notices = noticeRepository.findAllActiveNotices();
+        if (notices != null ) {
+            return ResponseEntity.ok()
+                    .cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+                    .body(notices);
+        }else {
+            return null;
+        }
+    }
+
+}
