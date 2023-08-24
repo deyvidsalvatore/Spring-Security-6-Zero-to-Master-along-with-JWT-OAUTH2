2983c5c9fa24b7f1a9e7efe578d78c6df5bebef7
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-08-24 15:01:00 -0300

    section 12 code

---
+++ section012/springoauth2/springoauth2/src/main/java/com/deyvidsalvatore/springoauth2/Springoauth2Application.java
@@ -0,0 +1,13 @@
+package com.deyvidsalvatore.springoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springoauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springoauth2Application.class, args);
+	}
+
+}
---
+++ section012/springoauth2/springoauth2/src/test/java/com/deyvidsalvatore/springoauth2/Springoauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deyvidsalvatore.springoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springoauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
